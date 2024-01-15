f2b6545db8e800336e4a4f945b5686ab81eb8fc7
Emil <emilschultze@online.de>
2024-01-15 11:55:42 +0100

    Rafactoring: Changes Folder Strucure Moves dart files for home and menu screen to sepret folders moves page_view screen to router folder

--- app/lib/src/routing/page_view_screen.dart
+++ app/lib/src/routing/page_view_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ri_go_demo/src/features/events/presentation/events_screen.dart';
-import 'package:ri_go_demo/src/features/rest_crud_demo/presentation/home_screen.dart';
-import 'package:ri_go_demo/src/features/rest_crud_demo/presentation/menu_screen.dart';
+import 'package:ri_go_demo/src/features/home/presentation/home_screen.dart';
+import 'package:ri_go_demo/src/features/menu/presentation/menu_screen.dart';
 
 class MyPageView extends ConsumerStatefulWidget {
   const MyPageView({
