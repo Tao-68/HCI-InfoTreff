bfada6a2c16ff9c8fe0484ae59c10aa50d2f29f2
Emil <emilschultze@online.de>
2024-01-21 13:14:31 +0100

    Adds_ UI for filter PopUp

--- app/lib/src/pop_ups/presentation/filter_popup.dart
+++ app/lib/src/pop_ups/presentation/filter_popup.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -8,13 +10,82 @@ class FilterPopUp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    return Stack(
+    return Container(
+      decoration: BoxDecoration(
+          color: theme.colorScheme.primary,
+          border: Border.all(width: 2),
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      padding: EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Filter',
+                style: TextStyle(
+                  color: theme.colorScheme.onPrimary,
+                  fontSize: 25,
+                ),
+              ),
+              CloseButton(theme: theme),
+            ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  width: 1.5,
+                  color: theme.colorScheme.onPrimary,
+                ),
+              ),
+            ),
+          ),
+          MenuFilter(title: 'New', theme: theme),
+          MenuFilter(title: 'Vegan', theme: theme),
+          MenuFilter(title: 'Vegetarian', theme: theme),
+          MenuFilter(title: 'Gluten free', theme: theme),
+          MenuFilter(title: 'Lactose free', theme: theme),
+        ],
+      ),
+    );
+  }
+}
+
+class MenuFilter extends ConsumerStatefulWidget {
+  const MenuFilter({required this.title, required this.theme, super.key});
+  final String title;
+
+  final ThemeData theme;
+  @override
+  ConsumerState<MenuFilter> createState() => _MenuFilterState();
+}
+
+class _MenuFilterState extends ConsumerState<MenuFilter> {
+  // Show or hide the content
+  bool? _checked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          alignment: Alignment.topRight,
-          child: CloseButton(theme: theme),
+        Text(
+          widget.title,
+          style: TextStyle(
+            color: widget.theme.colorScheme.onPrimary,
+            fontSize: 16,
+          ),
         ),
+        Checkbox(
+          value: _checked,
+          onChanged: (bool? value) {
+            setState(() {
+              _checked = value;
+            });
+          },
+        )
       ],
     );
   }
@@ -33,14 +104,14 @@ class CloseButton extends ConsumerWidget {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        border: Border.all(width: 2, color: theme.colorScheme.primary),
-        color: theme.colorScheme.onSecondary,
+        border: Border.all(width: 2, color: theme.colorScheme.onSecondary),
+        color: theme.colorScheme.secondary,
       ),
       child: IconButton(
         onPressed: () => context.pop(),
         icon: Icon(
           Icons.close,
-          color: theme.colorScheme.primary,
+          color: theme.colorScheme.onSecondary,
         ),
         iconSize: 40,
       ),
